dd67fa5b42d2e7064d6b43096ee41ec7f2065939
picupiee.id <picupiee.id@gmail.com>
2025-01-22 17:59:39 +0000

    pushing new Home.tsx

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { BlurView } from "expo-blur";
 
 export default function Home() {
   return (
     <View className="h-full">
-      <View className="flex flex-col items-center justify-center mt-10 py-5">
+      <View className="flex flex-col items-center justify-center mt-10">
         <Text className="bg-orange-400 rounded-full py-2 px-8 text-center text-white text-2xl font-bold">
           FooLine
         </Text>
