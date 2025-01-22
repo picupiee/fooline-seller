38eba6853f2e268511e11890674013177a20ca49
picupiee <picupiee.id@gmail.com>
2025-01-23 00:25:14 +0700

    first update, first page of home

---
+++ app/home.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Home() {
+  return (
+    <View className="h-full">
+      <View className="flex flex-col items-center justify-center mt-10 py-5">
+        <Text className="bg-orange-400 rounded-full py-2 px-8 text-center text-white text-2xl font-bold">
+          FooLine
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
