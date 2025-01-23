38a207a5d14aeacf8e9785c18b39ed8f25abbde7
picupiee.id <picupiee.id@gmail.com>
2025-01-23 14:47:58 +0000

    updating HeroSection

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,30 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, ImageBackground } from "react-native";
 import { BlurView } from "expo-blur";
 
 export default function HeroSection() {
+
+  const [tagline, setTagline] = useState("");
+  const [hourGreeting, setHourGreeting] = useState("Morning");
+
+  useEffect(() => {
+    const date = new Date();
+    const hour = date.getHours();
+      if (hour < 12) {
+         setTagline("It is a good time for a breakfast !");
+         setHourGreeting("Morning")
+      } else if (hour < 17) {
+        setTagline("It is a good time for a lunch !");
+        setHourGreeting("Afternoon")
+      } else {
+        setTagline("Let's end the day with good dinner from home !");
+        setHourGreeting("Evening")
+      }
+  }, []);
+
   return (
     <View>
-      <ImageBackground
+      {/* <ImageBackground
         source={require("@/assets/images/partial-react-logo.png")}
-        className="w-full h-80"
-      >
-        <View className="flex-1 items-center justify-center">
+        className="w-full h-50 p-2"
+      > */}
+        <View className="flex items-start justify-center p-0">
           {/* <BlurView
-            intensity={20}
+            intensity={40}
             experimentalBlurMethod="dimezisBlurView"
             tint="light"
-            className="p-10 overflow-hidden rounded-lg"
+            className="p-2 overflow-hidden rounded-lg"
           > */}
-          <Text className="text-black font-bold text-3xl">
-            Welcome to FooLine
-          </Text>
-          <Text className="text-black font-semibold text-lg">
-            Neighborhood Food Marketplace
-          </Text>
+          <View className="p-4">
+            <Text className="text-black font-bold text-[40px]">
+              Good {hourGreeting}
+            </Text>
+            <Text className="text-black font-semibold text-xl">
+              {tagline}
+            </Text>
+          </View>
+            {/* <Text className="text-black font-semibold text-xl">
+            Explore and order your favorite food !
+          </Text> */}
           {/* </BlurView> */}
         </View>
-      </ImageBackground>
+      {/* </ImageBackground> */}
     </View>
   );
 }
