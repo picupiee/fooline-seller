b89540e352a9e2f4df422366fbfe499e633f29b3
picupiee <picupiee.id@gmail.com>
2025-01-26 19:08:57 +0700

    updating index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,16 +4,19 @@ import FeaturedProducts from "../components/FeaturedProducts";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
 import ProductList from "../components/ProductList";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <ScrollView className="flex-1 bg-white">
-      {/* Components */}
-      <HeroSection />
-      <FeaturedProducts />
-      <SearchBar />
-      <Categories />
-      <ProductList />
-    </ScrollView>
+    <SafeAreaProvider>
+      <ScrollView className="flex-1 bg-white">
+        {/* Components */}
+        <HeroSection />
+        <FeaturedProducts />
+        <SearchBar />
+        <Categories />
+        <ProductList />
+      </ScrollView>
+    </SafeAreaProvider>
   );
 }
